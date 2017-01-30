585b98129da7e11542c52b491d5bbe9a023c70f1
Chetan Gorkal <chetankumar.1510@gmail.com>
2017-01-30 12:37:49 +0530

    excel reader sample

---
+++ utilities/src/me/jnvc/alumni/bean/AlumniMember.java
@@ -0,0 +1,43 @@
+package me.jnvc.alumni.bean;
+
+public class AlumniMember {
+	private String role;
+	private String name;
+	private String email;
+	private double contact;
+
+	public String getRole() {
+		return role;
+	}
+
+	public void setRole(String role) {
+		this.role = role;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public double getContact() {
+		return contact;
+	}
+
+	public void setContact(double contact) {
+		this.contact = contact;
+	}
+
+	
+
+}
